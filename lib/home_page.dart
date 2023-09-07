29e50b5547bd5b162b2a2488acc6ce325d22efc8
PuneetShetty19 <puneet.shetty@appyhigh.com>
2023-09-08 00:32:13 +0530

    home page update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,32 +9,63 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final list = [];
+
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
     return SafeArea(
         child: Scaffold(
       body: Container(
-        padding: EdgeInsets.all(20),
+        width: width,
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            TopRow(),
-            SizedBox(
+            const TopRow(),
+            const SizedBox(
               height: 45,
             ),
-            Container(
-              height: height * 0.25,
-              child: Image.asset('assets/bg.png'),
-            ),
+            Stack(children: [
+              Container(
+                height: height * 0.25,
+                width: width,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(40),
+                  child: Card(
+                      child: Image.asset(
+                    'assets/bg_map.jpg',
+                    fit: BoxFit.cover,
+                  )),
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: Container(
+                  height: height * 0.25,
+                  width: width,
+                  color: Colors.black.withOpacity(0.3),
+                ),
+              )
+            ]),
             // ElevatedButton(
             //   onPressed: () {
             //     Navigator.of(context).push(MaterialPageRoute(
             //       builder: (context) =>
             //           MapViewer(assetPath: 'assets/image2.png'),
-            //     ));
+            //     ));lo
             //   },
             //   child: Text('Open Full-Screen Image'),
             // ),
+            ListView.separated(
+                itemBuilder: (context, index) {
+                  return list[index];
+                },
+                separatorBuilder: (context, index) {
+                  return SizedBox(
+                    height: 18,
+                  );
+                },
+                itemCount: list.length)
           ],
         ),
       ),
