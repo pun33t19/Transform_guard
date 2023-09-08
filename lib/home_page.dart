d6b1362b2480a340f1bb940369858941e86d2017
PuneetShetty19 <puneet.shetty@appyhigh.com>
2023-09-08 21:11:07 +0530

    home page update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,13 +20,14 @@ class HomeScreen extends StatelessWidget {
         width: width,
         padding: const EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const TopRow(),
             const SizedBox(
               height: 45,
             ),
             Stack(children: [
-              Container(
+              SizedBox(
                 height: height * 0.25,
                 width: width,
                 child: ClipRRect(
@@ -43,10 +44,63 @@ class HomeScreen extends StatelessWidget {
                 child: Container(
                   height: height * 0.25,
                   width: width,
-                  color: Colors.black.withOpacity(0.3),
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Text(
+                  "View in Map",
+                  style: GoogleFonts.poppins(
+                      fontSize: 22, fontWeight: FontWeight.bold),
                 ),
               )
             ]),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: height * 0.25,
+              width: width,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: Card(
+                  color: Color.fromARGB(255, 238, 235, 235),
+                  elevation: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "View all\nPower Ways",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.poppins(
+                              fontSize: 22, fontWeight: FontWeight.w500),
+                        ),
+                        Image.asset("assets/tranformer_img.png")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Alert Zone",
+              style: GoogleFonts.poppins(
+                  fontSize: 22,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400),
+            ),
+            CustomListItem(
+                text1: "Savali Sector Sangli",
+                text2: "Power Way 2",
+                text3: "Avg height - 200.87m")
             // ElevatedButton(
             //   onPressed: () {
             //     Navigator.of(context).push(MaterialPageRoute(
@@ -56,16 +110,6 @@ class HomeScreen extends StatelessWidget {
             //   },
             //   child: Text('Open Full-Screen Image'),
             // ),
-            ListView.separated(
-                itemBuilder: (context, index) {
-                  return list[index];
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(
-                    height: 18,
-                  );
-                },
-                itemCount: list.length)
           ],
         ),
       ),
--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -17,31 +17,27 @@ class CustomListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius:
-                BorderRadius.circular(12.0), // Adjust the radius as needed
-            border: Border.all(
-              color: Colors.green, // Green border color
-              width: 2.0, // Border width
-            ),
-          ),
-          child: ListTile(
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(text1),
-                Text(text2),
-                Text(text3),
-              ],
-            ),
-            // trailing: Image.asset(imagePath),
-            onTap: onTap,
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius:
+            BorderRadius.circular(12.0), // Adjust the radius as needed
+        border: Border.all(
+          color: Colors.green, // Green border color
+          width: 2.0, // Border width
         ),
+      ),
+      child: ListTile(
+        leading: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(text1),
+            Text(text2),
+            Text(text3),
+          ],
+        ),
+        // trailing: Image.asset(imagePath),
+        onTap: onTap,
       ),
     );
   }
